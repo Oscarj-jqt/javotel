bca561827f41355e54060ee993df4f7d236cdabe
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-01-13 23:06:18 +0100

    Creating Auth Service & Calling Signup User API

--- HotelWeb/src/app/auth/components/register/register.component.ts
+++ HotelWeb/src/app/auth/components/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth/auth.service';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -13,7 +16,10 @@ export class RegisterComponent {
   registerForm!: FormGroup;
 
   // Inject FormBuilder to simplify form creation
-  constructor(private fb: FormBuilder,){}
+  constructor(private fb: FormBuilder,
+            private authService: AuthService,
+            private message: NzMessageService,
+            private router: Router){}
 
   ngOnInit(){
     // Initialize the reactive form with form controls and validators
@@ -21,6 +27,17 @@ export class RegisterComponent {
       email: [null, [Validators.email, Validators.required]],
       password: [null, Validators.required],
       name: [null, Validators.required]
+    })
+  }
+
+  submitForm(){
+    this.authService.register(this.registerForm.value).subscribe(res=> {
+      if(res.id != null) {
+        this.message.success("Inscription réussie", { nzDuration: 5000 });
+        this.router.navigateByUrl("/");
+      } else {
+        this.message.error(`${res.message}`, { nzDuration: 5000 })
+      }
     })
   }
 

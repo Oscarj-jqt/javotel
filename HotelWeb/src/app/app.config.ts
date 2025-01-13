86764f003b34db653239bb67e13ebc0913d6f03b
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-01-13 20:21:21 +0100

    Create Sidebar & Signup User Page with Reactive Form

--- HotelWeb/src/app/app.config.ts
+++ HotelWeb/src/app/app.config.ts
@@ -1,9 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
-import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { routes } from './app.routing.modules';
+import { fr_FR, en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
+import fr from '@angular/common/locales/fr';
 import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -12,5 +13,5 @@ import { provideHttpClient } from '@angular/common/http';
 registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideNzI18n(en_US), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideNzI18n(fr_FR), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient()]
 };
--- HotelWeb/src/app/app.modules.ts
+++ HotelWeb/src/app/app.modules.ts
@@ -1,20 +1,19 @@
-import { registerLocaleData } from "@angular/common";
-import en from "@angular/common/locales/en";
-import fr from "@angular/common/locales/fr";
-import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { en_US, fr_FR, NZ_I18N } from "ng-zorro-antd/i18n";
-import { HttpClientModule } from "@angular/common/http";
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { DemoNgZorroAntdModule } from "./DemoNgZorroAntdModule";
-import { NzLayoutModule } from 'ng-zorro-antd/layout';
-import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import fr from '@angular/common/locales/fr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { en_US, fr_FR, NZ_I18N } from 'ng-zorro-antd/i18n';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DemoNgZorroAntdModule } from './DemoNgZorroAntdModule';
+import { AppRoutingModule } from './app.routing.modules';
 
 
 
-registerLocaleData(fr);
+registerLocaleData(en);
 
 
 @NgModule({
@@ -23,18 +22,18 @@ registerLocaleData(fr);
     ],
     imports: [
         BrowserModule,
-        FormsModule,
+        AppRoutingModule,
+        FormsModule, 
         HttpClientModule,
         BrowserAnimationsModule,
         DemoNgZorroAntdModule,
-        NzLayoutModule,   
-        NzMenuModule,  
         ReactiveFormsModule
     ],
     providers: [
-        { provide: NZ_I18N, useValue: fr_FR}
+        { provide: NZ_I18N, useValue: en_US}
     ],
-    bootstrap: [AppComponent]
+    bootstrap: [AppComponent],
+    schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule { }
 

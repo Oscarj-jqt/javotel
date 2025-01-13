bd47b9bb38850459a523d0c34961d1fb70d9ecc2
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-01-13 14:41:16 +0100

    Add WebSecurityConfiguration: configure security to allow '/api/auth/**' and disable CSRF

---
+++ HotelServer/src/main/java/com/example/HotelServer/configs/WebSecurityConfiguration.java
@@ -0,0 +1,28 @@
+package com.example.HotelServer.configs;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+// Enables Spring Security in the application
+@EnableWebSecurity
+@RequiredArgsConstructor
+
+public class WebSecurityConfiguration {
+
+    // Declares this method as a Bean, allowing Spring to manage it
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // Disables CSRF protection (useful for APIs, especially during development)
+        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(request->
+                // Allows anyone to access endpoints under '/api/auth/**' without authentication
+                request.requestMatchers("/api/auth/**").permitAll());
+
+        return http.build();
+    }
+}

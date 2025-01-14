f89ac63d0e62b53e3e3910d6902fe3ce3b43b735
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-01-14 23:25:32 +0100

    Creating Login Page & Calling Login API

--- HotelWeb/src/app/app.component.ts
+++ HotelWeb/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { UserStorageService } from './auth/services/storage/user-storage.service';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,25 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'HotelWeb';
+
+  isCustomerLoggedIn:boolean = UserStorageService.isCustomerLoggedIn();
+  isAdminLoggedIn:boolean = UserStorageService.isAdminLoggedIn();
+
+  constructor(private router: Router){}
+
+  ngOnInit(){
+    this.router.events.subscribe(event=>{
+      if(event.constructor.name === " NavagationEnd"){
+        this.isCustomerLoggedIn = UserStorageService.isCustomerLoggedIn();
+        this.isAdminLoggedIn = UserStorageService.isAdminLoggedIn();
+      }
+    })
+  }
+
+  logout(){
+    UserStorageService.signOut();
+    this.router.navigateByUrl('/')
+  }
 }
+
+

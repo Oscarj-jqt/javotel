714b67ec54b9c7bdbf5b780b44a13c80535e08b9
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-01-14 21:18:29 +0100

    Creating Login Page & Calling Login API

--- HotelWeb/src/app/app.routing.modules.ts
+++ HotelWeb/src/app/app.routing.modules.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { RegisterComponent } from './auth/components/register/register.component';
+import { LoginComponent } from './auth/components/login/login.component';
 
 
 export const routes: Routes = [
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: '', component: LoginComponent }
   
 ];
 

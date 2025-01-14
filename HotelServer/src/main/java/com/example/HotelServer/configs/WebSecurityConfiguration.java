547e5457364cc52328de31f42052441e02e92f2d
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-01-14 18:17:22 +0100

    Add AuthenticationManager bean in WebSecurityConfiguration

--- HotelServer/src/main/java/com/example/HotelServer/configs/WebSecurityConfiguration.java
+++ HotelServer/src/main/java/com/example/HotelServer/configs/WebSecurityConfiguration.java
@@ -3,6 +3,8 @@ package com.example.HotelServer.configs;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -24,5 +26,12 @@ public class WebSecurityConfiguration {
                 request.requestMatchers("/api/auth/**").permitAll());
 
         return http.build();
+    }
+
+    //- Define a @Bean method to expose AuthenticationManager.
+    @Bean
+    // Use AuthenticationConfiguration to retrieve the AuthenticationManager instance.
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }

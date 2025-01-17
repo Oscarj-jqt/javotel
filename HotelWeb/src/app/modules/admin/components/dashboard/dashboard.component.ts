9e2875bba8799a3935e915a9086f3891f87dab51
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-01-17 23:57:14 +0100

    Implementing Pagination & Showing Rooms Data

--- HotelWeb/src/app/modules/admin/components/dashboard/dashboard.component.ts
+++ HotelWeb/src/app/modules/admin/components/dashboard/dashboard.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { AdminService } from '../../admin-services/admin.service';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
 
+  currentPage = 1;
+  rooms = [];
+  constructor(private adminService: AdminService,
+    private message: NzMessageService,
+  ){
+    this.getRooms();
+  }
+
+  getRooms(){
+    this.adminService.getRooms(this.currentPage -1).subscribe(res=>{
+      console.log(res);
+      this.rooms = res;
+    })
+  }
 }

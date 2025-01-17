434ad179f0bd683d7b49418b083a05b26de5b7c4
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-01-18 00:43:24 +0100

    config

--- HotelWeb/src/app/modules/admin/components/dashboard/dashboard.component.ts
+++ HotelWeb/src/app/modules/admin/components/dashboard/dashboard.component.ts
@@ -11,6 +11,7 @@ export class DashboardComponent {
 
   currentPage = 1;
   rooms = [];
+    total:any;
   constructor(private adminService: AdminService,
     private message: NzMessageService,
   ){
@@ -21,6 +22,7 @@ export class DashboardComponent {
     this.adminService.getRooms(this.currentPage -1).subscribe(res=>{
       console.log(res);
       this.rooms = res;
+      this.total = res.totalPages * 1;
     })
   }
 }

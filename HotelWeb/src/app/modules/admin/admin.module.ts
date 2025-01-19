14a696bfbe096de1ca5212f071152601593cbeac
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-01-19 20:21:05 +0100

    Calling Delete Room API with Confirm Model

--- HotelWeb/src/app/modules/admin/admin.module.ts
+++ HotelWeb/src/app/modules/admin/admin.module.ts
@@ -14,7 +14,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   declarations: [
     AdminComponent,
     DashboardComponent,
-    PostRoomComponent
+    PostRoomComponent,
   ],
   imports: [
     CommonModule,
--- HotelWeb/src/app/modules/customer/customer.module.ts
+++ HotelWeb/src/app/modules/customer/customer.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DemoNgZorroAntdModule } from '../../DemoNgZorroAntdModule';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    DemoNgZorroAntdModule
   ]
 })
 export class CustomersModule { }
